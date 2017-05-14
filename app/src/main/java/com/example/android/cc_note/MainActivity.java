7e67bb0effbfa42efecb32aaf86fb6464707e10c
hyomNam <skagbghk@gmail.com>
2017-05-14 14:40:14 +0900

    Add Crop

--- app/src/main/java/com/example/android/cc_note/MainActivity.java
+++ app/src/main/java/com/example/android/cc_note/MainActivity.java
@@ -1,31 +1,37 @@
 package com.example.android.cc_note;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
 
-    private Button btnTakePicture;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 2;
+    static final int REQUEST_IMAGE_CROP = 3;
+
+    private Button btnTakePicture, btnGetAlbum;
     private ImageView ivCapture;
+    Uri photoURI = null, albumURI = null;
     private String imagePath;
+    Boolean album = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,104 +40,125 @@ public class MainActivity extends AppCompatActivity {
 
         btnTakePicture = (Button) findViewById(R.id.btntakePicture);
         ivCapture = (ImageView) findViewById(R.id.ivPicture);
+        btnGetAlbum = (Button) findViewById(R.id.btnGetAlbum);
 
-        btnTakePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isExistsCameraApplication()){
-                    Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File picture = savePictureFile();
-
-                    if(picture != null){
-                        cameraApp.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(picture));
-
-//                        startActivityForResult(cameraApp, 10000);
-                        setResult(10000, cameraApp);
-                        finish();
-
-                    }
-                }
-
-            }
-        });
+        btnTakePicture.setOnClickListener(this);
+        btnGetAlbum.setOnClickListener(this);
     }
 
-    private boolean isExistsCameraApplication(){
-
-        PackageManager packageManager = getPackageManager();
+    public void dispatchTakePictureIntent(){
         Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        List<ResolveInfo> cameraApps = packageManager.queryIntentActivities(cameraApp, PackageManager.MATCH_DEFAULT_ONLY);
-
-        return cameraApps.size() > 0;
-    }
-
-    private File savePictureFile(){
-        PermissionRequester.Builder requester = new PermissionRequester.Builder(this);
-
-        int result = requester.create()
-                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE, 20000, new PermissionRequester.OnClickDenyButtonListener(){
-                    @Override
-                    public void onClick(Activity activity){
-
-                    }
-                });
-
-        if(result == PermissionRequester.ALREADY_GRANTED || result == PermissionRequester.REQUEST_PERMISSION){
-            //파일 이름 설정
-            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String fileName = "IMG_" + timestamp;
-
-            //파일 저장 위치 설정
-            File pictureStorage = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MYAPP/");
-
-            //저장될 폴더가 없으면 생성
-            if( !pictureStorage.exists() ){
-                pictureStorage.mkdirs();
+        if(cameraApp.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            } catch(IOException ex){
+                Toast.makeText(getApplicationContext(), "createImageFile Fail", Toast.LENGTH_SHORT).show();
             }
 
-            try{
-                File file = File.createTempFile(fileName, ".jpg", pictureStorage);
+            if(photoFile != null){
+                Toast.makeText(getApplicationContext(), imagePath, Toast.LENGTH_SHORT).show();
+                imagePath = photoFile.getAbsolutePath();
+                cameraApp.putExtra(MediaStore.EXTRA_OUTPUT, imagePath);
+                this.setResult(RESULT_OK, cameraApp);
+                startActivityForResult(cameraApp, REQUEST_IMAGE_CAPTURE) ;
 
-                // 사진 파일 절대 경로 가져옴
-                imagePath = file.getAbsolutePath();
-
-                //찍힌 사진을 갤러리에 저장
-                Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                File f = new File (imagePath);
-                Uri contentUri = Uri.fromFile(f);
-                mediaScanIntent.setData(contentUri);
-                this.sendBroadcast(mediaScanIntent);
-
-                return file;
-            } catch(Exception e){
-                e.printStackTrace();
             }
         }
-        return null;
+    }
+
+    private File createImageFile() throws IOException{
+        SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd-HH-mm-ss", Locale.KOREA );
+        Date currentTime = new Date ( );
+        String imageFileName = "ccnote_" + formatter.format (currentTime) + ".jpg";
+
+        File storageDir = new File(Environment.getExternalStorageDirectory(), imageFileName);
+        imagePath = storageDir.getAbsolutePath();
+        return storageDir;
+    }
+
+    private void doTakeAlbumAction(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        this.setResult(RESULT_OK, intent);
+        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+    }
+
+
+    private void cropImage(){
+        Intent cropIntent = new Intent("com.android.camera.action.CROP");
+        cropIntent.setDataAndType(photoURI, "image/*");
+        cropIntent.putExtra("scale", true);
+
+        if(album == false){
+            cropIntent.putExtra("output", photoURI);
+        }
+        else if (album == true){
+            cropIntent.putExtra("output", albumURI);
+        }
+        this.setResult(RESULT_OK, cropIntent);
+        startActivityForResult(cropIntent, REQUEST_IMAGE_CROP);
     }
 
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
 
+        if(resultCode != RESULT_OK){
+            Toast.makeText(getApplicationContext(), "onActivityResult : RESULT_NOT_OK - " + requestCode, Toast.LENGTH_SHORT).show();
+        }
+        else{
 
-        if(requestCode == 10000 && resultCode == RESULT_OK){
-//            String key = data.getStringExtra("key");
+            switch(requestCode){
+                case REQUEST_TAKE_PHOTO:
+                    Toast.makeText(getApplicationContext(), "onActivityResult : 1", Toast.LENGTH_SHORT).show();
+                    album = true;
+                    File albumFile = null;
+                    try{
+                        albumFile = createImageFile();
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                    if(albumFile != null){
+                        albumURI = Uri.fromFile(albumFile);
+                    }
+                    photoURI = data.getData();
 
+                case REQUEST_IMAGE_CAPTURE:
+                    Toast.makeText(getApplicationContext(), "onActivityResult : 2", Toast.LENGTH_SHORT).show();
+                    cropImage();
+                    break;
 
-            BitmapFactory.Options factory = new BitmapFactory.Options();
-            factory.inJustDecodeBounds = true;
+                case REQUEST_IMAGE_CROP:
+                    Toast.makeText(getApplicationContext(), "onActivityResult : 3", Toast.LENGTH_SHORT).show();
+                    Bitmap photo = BitmapFactory.decodeFile(photoURI.getPath());
+                    ivCapture.setImageBitmap(photo);
 
-            BitmapFactory.decodeFile(imagePath);
+                    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    if(album == false){
+                        mediaScanIntent.setData(photoURI);
+                    }
+                    else if(album == true){
+                        album = false;
+                        mediaScanIntent.setData(albumURI);
+                    }
+                    this.sendBroadcast(mediaScanIntent);
+                    break;
+            }
+        }
+    }
 
-            factory.inJustDecodeBounds = false;
-            factory.inPurgeable = true;
-
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, factory);
-            ivCapture.setImageBitmap(bitmap);
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case (R.id.btntakePicture):
+                dispatchTakePictureIntent();
+                break;
+            case (R.id.btnGetAlbum):
+                doTakeAlbumAction();
+                break;
         }
     }
 }
